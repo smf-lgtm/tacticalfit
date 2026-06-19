459c34723ce227463bde4bf08c54252b6a4567d4
smf-lgtm <smfrancis1709@gmail.com>
2026-06-19 13:01:07 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE_NAME="tacticalfit-v1";const ASSETS=["./","./index.html","./app.js","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
